39a8b64bf29c26e4e9d0518e2f5fa79fd01c990c
Arzel John Zolina <Arzeljrz17@gmail.com>
2025-02-11 17:52:54 +0800

    fix multi auth design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,76 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<body>
+    <div class="bg-gray-50 font-[sans-serif]">
+        <div class="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+            <div class="max-w-md w-full">
+                <a href="javascript:void(0)"><img
+                        src="{{ asset('assets/images/logo.png' ) }}" alt="logo" class='w-40 mb-8 mx-auto block' />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
                 </a>
-            @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                <div class="p-8 rounded-2xl bg-white shadow">
+                    <h2 class="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                    <form action="{{ route('login') }}" class="mt-8 space-y-4" method="POST">
+                        @csrf
+
+                        <div>
+                            <label class="text-gray-800 text-sm mb-2 block">Username</label>
+                            <div class="relative flex items-center">
+                                <input name="username" id="username" type="text" required class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter username" />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
+                                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                                    <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
+                                </svg>
+                            </div>
+                        </div>
+
+                        <div>
+                            <label class="text-gray-800 text-sm mb-2 block">Password</label>
+                            <div class="relative flex items-center">
+                                <input id="password" name="password" type="password" required class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600" placeholder="Enter password" />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                                </svg>
+                            </div>
+                        </div>
+
+                        <div class="flex flex-wrap items-center justify-between gap-4">
+                            <div class="flex items-center">
+                                <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                                <label for="remember-me" class="ml-3 block text-sm text-gray-800">
+                                    {{ __('Remember me') }}
+                                </label>
+                            </div>
+                            <div class="text-sm">
+                                @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}" class=" text-blue-600 hover:underline font-semibold">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="!mt-8">
+                            <button type="submit" class="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+                                {{ __('Log in') }}
+                            </button>
+                        </div>
+                        <p class="text-gray-800 text-sm !mt-8 text-center">Don't have an account? <a href="{{ route('register') }}" class="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Register here</a></p>
+                    </form>
+                </div>
+            </div>
         </div>
-    </form>
-</x-guest-layout>
+    </div>
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,147 +8,84 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-100 flex items-center justify-center min-h-screen">
-    <div class="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-        <h2 class="text-center text-2xl font-bold mb-6">Create Account</h2>
+<body class="bg-gray-100">
+
+    <div class="max-w-4xl max-sm:max-w-lg mx-auto font-[sans-serif] p-6 border-white border-2 rounded-lg bg-white">
+        <div class="text-center mb-12 sm:mb-16">
+            <a href="javascript:void(0)">
+                <img src="{{ asset('assets/images/logo.png' ) }}" alt="logo" class='w-40 inline-block' />
+            </a>
+            <h4 class="text-gray-600 text-base mt-6">Create an Account</h4>
+        </div>
 
         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
             @csrf
-            <!-- Name Fields -->
-            <div class="grid grid-cols-3 gap-4">
-                <div class="relative">
-                    <label for="firstname" class="block font-medium">Firstname</label>
-                    <div class="relative">
-                        <i class="fas fa-user absolute left-3 top-3 text-gray-500"></i>
-                        <input id="firstname" type="text" name="firstname" required autofocus
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
+            <div class="grid sm:grid-cols-2 gap-6">
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">First Name</label>
+                    <input type="text" name="firstname" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter first name" required />
                 </div>
-
-                <div class="relative">
-                    <label for="middlename" class="block font-medium">Middle Name</label>
-                    <div class="relative">
-                        <i class="fas fa-user absolute left-3 top-3 text-gray-500"></i>
-                        <input id="middlename" type="text" name="middlename"
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Middle Name</label>
+                    <input type="text" name="middlename" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter middle name" />
                 </div>
-
-                <div class="relative">
-                    <label for="lastname" class="block font-medium">Lastname</label>
-                    <div class="relative">
-                        <i class="fas fa-user absolute left-3 top-3 text-gray-500"></i>
-                        <input id="lastname" type="text" name="lastname" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Last Name</label>
+                    <input type="text" name="lastname" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter last name" required />
                 </div>
-            </div>
-
-            <!-- Contact Fields -->
-            <div class="grid grid-cols-3 gap-4 mt-4">
-                <div class="relative">
-                    <label for="email" class="block font-medium">Email</label>
-                    <div class="relative">
-                        <i class="fas fa-envelope absolute left-3 top-3 text-gray-500"></i>
-                        <input id="email" type="email" name="email" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Email</label>
+                    <input type="email" name="email" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter email" required />
                 </div>
-
-                <div class="relative">
-                    <label for="phone_number" class="block font-medium">Phone Number</label>
-                    <div class="relative">
-                        <i class="fas fa-phone absolute left-3 top-3 text-gray-500"></i>
-                        <input id="phone_number" type="text" name="phone_number" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Phone Number</label>
+                    <input type="text" name="phone_number" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter phone number" required />
                 </div>
-
-                <div class="relative">
-                    <label for="city" class="block font-medium">City</label>
-                    <div class="relative">
-                        <i class="fas fa-city absolute left-3 top-3 text-gray-500"></i>
-                        <select id="city" name="city" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                            <option value="">Select City</option>
-                        </select>
-                    </div>
+                <div>
+                    <label for="city" class="text-gray-600 text-sm mb-2 block">City</label>
+                    <select id="city" name="city" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" required>
+                        <option value="">Select City</option>
+                    </select>
+                </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Barangay</label>
+                    <select name="barangay" id="barangay" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" required>
+                        <option value="">Select Barangay</option>
+                    </select>
+                </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Street Address</label>
+                    <input type="text" name="street_address" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter street address" required />
+                </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Zipcode</label>
+                    <input type="text" name="zipcode" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter zipcode" required />
+                </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Username</label>
+                    <input type="text" name="username" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter username" required />
+                </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Password</label>
+                    <input type="password" name="password" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Enter password" required />
+                </div>
+                <div>
+                    <label class="text-gray-600 text-sm mb-2 block">Confirm Password</label>
+                    <input type="password" name="password_confirmation" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" placeholder="Confirm password" required />
                 </div>
             </div>
 
-            <!-- Address Fields -->
-            <div class="grid grid-cols-3 gap-4 mt-4">
-                <div class="relative">
-                    <label for="barangay" class="block font-medium">Barangay</label>
-                    <div class="relative">
-                        <i class="fas fa-map-marker-alt absolute left-3 top-3 text-gray-500"></i>
-                        <select id="barangay" name="barangay" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                            <option value="">Select Barangay</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="relative">
-                    <label for="street_address" class="block font-medium">Street Address</label>
-                    <div class="relative">
-                        <i class="fas fa-road absolute left-3 top-3 text-gray-500"></i>
-                        <input id="street_address" type="text" name="street_address" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
-                </div>
-
-                <div class="relative">
-                    <label for="zipcode" class="block font-medium">Zipcode</label>
-                    <div class="relative">
-                        <i class="fas fa-map-pin absolute left-3 top-3 text-gray-500"></i>
-                        <input id="zipcode" type="text" name="zipcode" required
-                            class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                    </div>
-                </div>
-            </div>
-
-            <!-- Account Details -->
-            <div class="mt-4 relative">
-                <label for="username" class="block font-medium">Username</label>
-                <div class="relative">
-                    <i class="fas fa-user-circle absolute left-3 top-3 text-gray-500"></i>
-                    <input id="username" type="text" name="username" required
-                        class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                </div>
-            </div>
-
-            <div class="mt-4 relative">
-                <label for="password" class="block font-medium">Password</label>
-                <div class="relative">
-                    <i class="fas fa-lock absolute left-3 top-3 text-gray-500"></i>
-                    <input id="password" type="password" name="password" required
-                        class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                </div>
-            </div>
-
-            <div class="mt-4 relative">
-                <label for="password_confirmation" class="block font-medium">Confirm Password</label>
-                <div class="relative">
-                    <i class="fas fa-lock absolute left-3 top-3 text-gray-500"></i>
-                    <input id="password_confirmation" type="password" name="password_confirmation" required
-                        class="pl-10 p-2 border w-full rounded-md focus:ring-2 focus:ring-blue-500">
-                </div>
-            </div>
-
-            <!-- Profile Verification -->
             <div class="mt-6">
-                <h3 class="font-bold text-lg">Profile Verification</h3>
-
-                <div class="mt-2 relative">
-                    <label for="valid_id" class="block font-medium">Valid ID</label>
-                    <input type="file" id="valid_id" name="valid_id" class="border w-full p-2 rounded-md">
+                <h3 class="font-bold text-lg text-gray-800">Profile Verification</h3>
+                <div class="mt-2">
+                    <label class="text-gray-600 text-sm mb-2 block">Valid ID</label>
+                    <input type="file" name="valid_id" class="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded focus:bg-transparent outline-blue-500 transition-all" required />
                 </div>
                 <div class="mt-4 relative">
-                    <label class="block font-medium">Identity Recognition</label>
+                    <label class="text-gray-600 text-sm mb-2 block">Identity Recognition</label>
 
                     <!-- Camera Button -->
-                    <button type="button" id="openCamera" class="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-md">
+                    <button type="button" id="openCamera" class="flex items-center gap-2 bg-gray-600 text-white px-4 py-2 rounded-md">
                         <i class="fas fa-camera"></i> Open Camera
                     </button>
 
@@ -173,21 +110,22 @@
                     <!-- Captured Image Preview -->
                     <div id="capturedImagePreview" class="mt-4 hidden">
                         <h4 class="font-medium">Captured Image:</h4>
-                        <img id="capturedImage" class="mt-2 w-32 h-32 object-cover rounded-md border">
+                        <img id="capturedImage" class="mt-2 w-32 h-32 object-cover rounded-md border"> <br>
                         <input type="file" id="identity_recognition" name="identity_recognition">
                     </div>
                 </div>
 
             </div>
 
-            <!-- Submit Button -->
-            <div class="mt-6">
-                <button type="submit" class="w-full bg-green-600 text-white p-3 rounded-lg hover:bg-green-700">
-                    <i class="fas fa-user-plus"></i> SIGN UP
+            <div class="mt-8">
+                <button type="submit" class="w-full py-3 px-6 text-sm tracking-wider rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+                    Sign up
                 </button>
             </div>
+
         </form>
     </div>
+
 
     <!-- Add FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -250,6 +188,9 @@
             document.getElementById("capturedImage").src = "";
             document.getElementById("identity_recognition").value = "";
         };
+    </script>
+
+    <script>
         document.addEventListener("DOMContentLoaded", function() {
             const citySelect = document.getElementById("city");
             const barangaySelect = document.getElementById("barangay");
@@ -261,7 +202,7 @@
                     citySelect.innerHTML = '<option value="">Select City</option>';
                     data.forEach((city) => {
                         let option = document.createElement("option");
-                        option.value = city.code; 
+                        option.value = city.code;
                         option.text = city.name;
                         citySelect.appendChild(option);
                     });
